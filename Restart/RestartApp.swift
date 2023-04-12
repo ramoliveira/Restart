e966950fcf5078eb4bf675bea7d203eb70464285
Ramón Almeida <ramon.almeida@southsystem.com.br>
2023-04-12 13:11:25 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ramón Dias de Oliveira Almeida on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
